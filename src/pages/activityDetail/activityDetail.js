e07e1cd2163759afcabd8e5c50e3b1fc99cd5009
lusssn <lusssn@126.com>
2019-11-24 20:21:04 +0800

    A 活动详情页*2

---
+++ src/pages/activityDetail/activityDetail.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    originator: {
+      accountId: 3244866041856,
+      avatar: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqMeko1pzUxUnIPzhYzdMYqw96hIOsdVg5liclMn94iaTsDRxuMVlq9T9QCKQugqWHKMTOvopaMoArA/132',
+      city: '深圳市',
+      college: '软件工程',
+      company: '腾讯',
+      name: '孙叶',
+      position: '产品经理',
+      recommondReason: '同学院',
+      school: '东南大学',
+      selfDesc: '产品设计师',
+    },
+  }
+})
