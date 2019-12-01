8724bc0538d1e5cc1ce007f7d699c426e8b9f283
Raaabbit <yangflo@foxmail.com>
2019-12-01 21:13:47 +0800

    feat：校友圈详情页面

--- src/pages/myAlumni/myAlumni.js
+++ src/pages/myAlumni/myAlumni.js
@@ -44,46 +44,4 @@ Page({
     ],
     messagePagination: { current: 1, total: 0 },
   },
-  onLoad() {
-    wxUtil.login().then(() => {
-      this.loadAlumniList()
-    })
-  },
-  onPullDownRefresh() {
-    this.loadAlumniList().then(() => {
-      wx.stopPullDownRefresh()
-    })
-  },
-  onReachBottom() {
-    const { total, current } = this.data.startedPagination
-    // 是否为最后一页
-    if (Math.ceil(total / PAGE_SIZE) > current) {
-      this.loadAlumniList(current + 1)
-    }
-  },
-  loadAlumniList(pageNo = 1) {
-    return Api.getMyAlumni({
-      accountId: app.global.accountId,
-      pageIndex: pageNo,
-      pageSize: PAGE_SIZE,
-    }).then(data => {
-      const { list, count } = data
-      this.setData({
-        startedList: pageNo === 1 ? list : this.data.startedList.concat(list),
-        startedPagination: {
-          current: pageNo,
-          total: count,
-        },
-      })
-    }, () => {})
-  },
-  // 校友圈详情
-  handleClickAlumniItem(e) {
-    const { id } = e.currentTarget.dataset;
-    wxUtil.navigateTo('alumniDetail', { id })
-  },
-  //创建群通讯录
-  handleClickCreate() {
-    wxUtil.navigateTo('createGroupBook')
-  }
 })
