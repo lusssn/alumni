f7e0485000e6431d32ccbe87ad33caccfad906d7
lusssn <lusssn@126.com>
2019-12-08 17:14:37 +0800

    U 圈子详情页优化

--- src/pages/hubDetail/hubDetail.js
+++ src/pages/hubDetail/hubDetail.js
@@ -1,7 +1,11 @@
 import wxUtil from '../../utils/wxUtil'
 
 Page({
+  data: {
+    activityList: [],
+  },
   handleToCreateActivity() {
+    // TODO 需要传入圈子id
     wxUtil.navigateTo('activityCreate')
   }
 })
